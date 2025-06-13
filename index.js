f56e99d28a5a6445c787bbdc36aafccf9d50e280
unknown <ruth.co.264@gmail.com>
2025-06-13 15:34:55 +0300

    Fixed path definition in index.js

--- index.js
+++ index.js
@@ -36,10 +36,14 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
 
   const path = require('path');
-  app.get('*', (req, res) => {
+
+  app.get('/*path', (req, res) => {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
+// app.get('*', (req, res) => {
+//res.sendFile(path.resolve('client', 'build', 'index.html'));
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
